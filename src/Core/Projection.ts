2e7bc5875f929d144f275fe92b3100e9b790561f
K024 <Kjy_@live.cn>
2020-08-07 20:20:06 +0800

    fastfall is slower

--- src/Core/Projection.ts
+++ src/Core/Projection.ts
@@ -1,27 +1,12 @@
 import { CenterX, FarLineZ, LaneInfY, LaneBottomY } from "./Constants"
-import { ratio } from "../Utils/Utils"
 
 /**
  * fall function
  * @param t 0: near, -1: far
  * @returns 0 -> 1, ratio of y
  */
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 function fall(t: number) {
     return Math.pow(1.1, 50 * t)
-}
-
-const falllist: number[] = []
-for (let i = 0; i < 100; i++) {
-    falllist.push(Math.pow(1.1, i - 50))
-}
-
-function fastfall(t: number) {
-    const tt = (t + 1) * 50
-    let i = Math.floor(tt)
-    if (i > 98) i = 98
-    else if (i < 0) i = 0
-    return ratio(i, i + 1, tt, falllist[i], falllist[i + 1])
 }
 
 /**
@@ -30,8 +15,7 @@ function fastfall(t: number) {
  * @param laneOffset Lane center x or other
  */
 export function projection(trackpos: number, laneOffset: number) {
-    // const f = fall(trackpos);
-    const f = fastfall(trackpos)
+    const f = fall(trackpos)
     const r = ((FarLineZ - 1) * f + 1) / FarLineZ
     return {
         x: CenterX + (laneOffset - CenterX) * r,
